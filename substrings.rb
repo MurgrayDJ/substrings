3d632c27572c9e672477dd9a9fad92a3d082d932
murgraydj <murgraydj@gmail.com>
2022-11-10 14:10:02 -0500

    Adds substrings and run program methods
    The substrings method returns a hash listing each substring from the
    passed in dictionary that matches the passed in string. The run program
    method passes the substring method a string and an array of strings.

--- substrings.rb
+++ substrings.rb
@@ -2,3 +2,22 @@
 # Date created: 11/10/2022 
 # Purpose: Given a word and a list of words, this program returns words from the list that are substrings.
 
+def substrings(main_string, string_array)
+    counter_hash = {}
+
+    string_array.each do |string|
+        occurences = main_string.downcase.scan(string.downcase)
+        unless occurences.empty?
+            counter_hash[string] = occurences.length
+        end
+    end
+    counter_hash
+end
+
+def run_program
+    main_string = "Howdy partner, sit down! How's it going?"
+    string_array = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+    puts substrings(main_string, string_array)
+end
+
+run_program
